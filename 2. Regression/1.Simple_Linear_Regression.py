72847ff25c3195573610234763c0c0426380d875
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-06 01:43:13 +0530

    Update 1.Simple_Linear_Regression.py

--- 2. Regression/1.Simple_Linear_Regression.py
+++ 2. Regression/1.Simple_Linear_Regression.py
@@ -5,3 +5,12 @@ Adarsh Razor
 06-06-2021 01:22AM
 """
 
+#importing the libraries
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('1.SLR_Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
