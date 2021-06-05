b538b672608e3ac817a15546bbaebfcd8e135123
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-06 02:13:53 +0530

    Update 1.Simple_Linear_Regression.py

--- 2. Regression/1.Simple_Linear_Regression.py
+++ 2. Regression/1.Simple_Linear_Regression.py
@@ -9,6 +9,7 @@ Adarsh Razor
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn
 
 #importing the dataset
 dataset = pd.read_csv('1.SLR_Salary_Data.csv')
@@ -16,7 +17,7 @@ X = dataset.iloc[:, :-1].values #independent variable
 Y = dataset.iloc[:, 1].values  #dependent variable
 
 #splitting data into testing and training set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/3, random_state=0 )
 
 
