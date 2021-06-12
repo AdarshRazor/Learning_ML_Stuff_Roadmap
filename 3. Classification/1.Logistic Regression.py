21bb6f9bff5e8422adbd577f06f6891151379116
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-13 02:03:03 +0530

    Update 1.Logistic Regression.py

--- 3. Classification/1.Logistic Regression.py
+++ 3. Classification/1.Logistic Regression.py
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
+# Detecting whether the user is going to buy the car based on age and salary
+dataset = pd.read_csv('1.LR_Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
@@ -21,7 +22,7 @@ y = dataset.iloc[:, 4].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling, For acurate prediction we are applying
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
