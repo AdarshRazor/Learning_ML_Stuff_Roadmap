0a41e754ae87b2d29110aefc1ed9dfcb98ec9c27
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-13 03:32:53 +0530

    Update 1.K-Means.py

--- 4. Clustering/1.K-Means.py
+++ 4. Clustering/1.K-Means.py
@@ -48,7 +48,7 @@ plt.show()
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter=300, n_init=10, random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
 
-# Visualising the clusters
+# Visualising the clusters [ Only for 2 dimension ]
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
