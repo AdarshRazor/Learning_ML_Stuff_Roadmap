851cfff063a79b16086d665f1f89cae1c2e6e067
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-13 01:50:28 +0530

    Update 6.Random_Forest_Regression.py

--- 2. Regression/6.Random_Forest_Regression.py
+++ 2. Regression/6.Random_Forest_Regression.py
@@ -11,15 +11,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('3.PR_Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
+# Again we are not splitting the dataset because of low training set
 """from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
+# Its on you to apply the feature scalling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -29,7 +31,8 @@ y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
 
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor = RandomForestRegressor(n_estimators = 10,    # no. of random tree you want to select
+                                  random_state = 0)     
 regressor.fit(X, y)
 
 # Predicting a new result
