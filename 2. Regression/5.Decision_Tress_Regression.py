da1f85f3b9534293899d6127a70054291e95e078
Adarsh Anand <adarshanshu7@gmail.com>
2021-06-13 01:45:17 +0530

    Update 5.Decision_Tress_Regression.py

--- 2. Regression/5.Decision_Tress_Regression.py
+++ 2. Regression/5.Decision_Tress_Regression.py
@@ -11,15 +11,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('3.PR_Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
+# Again we are not splitting the dataset because of low training set
 """from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
+# Its on you to apply the feature scalling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -28,7 +30,7 @@ sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
 
 # Fitting Decision Tree Regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor   # importing the library
 regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, y)
 
@@ -36,6 +38,7 @@ regressor.fit(X, y)
 y_pred = regressor.predict([[6.5]])
 
 # Visualising the Decision Tree Regression results (higher resolution)
+# In decision tree its taking the average hence the graph is coming like this.
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
